998d3492dc22b381f37950ff00d0f916cf7cf796
Dino Gradaskic <123075998+DinoGradaskic@users.noreply.github.com>
2023-02-04 12:18:53 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import { ChatEngine } from 'react-chat-engine';
+import React from 'react';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="89bb3a14-6c62-4a22-b678-b3449862bf55"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+
+export default App;
